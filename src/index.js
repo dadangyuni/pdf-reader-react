3f2c740d2a7de25b93d6b0e2b4fafe31203da3f8
dadang <dadanginuy@gmail.com>
2022-07-10 11:23:49 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
 import ReactDOM from 'react-dom';
 import App from './layouts';
-=======
-import ReactDOM from 'react-dom/client';
-import App from './views/viewer';
->>>>>>> origin/develop
 
 ReactDOM.render(<App />, document.getElementById('root'));
