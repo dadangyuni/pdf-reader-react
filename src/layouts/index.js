21dd4deb769efcb8d8982ef9b4264620f2acddfc
dadang <dadanginuy@gmail.com>
2022-07-25 16:06:40 +0700

    updated router

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import {Route, Routes} from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
-import PdfTurnjs from '../views/pdfTurnjs';
-import './styles/index.style.css'
+import './styles/index.style.css';
+
+const PdfTurnjs = React.lazy(()=> import('../views/pdfTurnjs'));
 
 /*eslint-disable*/
 const App = (props) => {
@@ -12,7 +13,10 @@ const App = (props) => {
       <Routes>
         <Route 
           path='/' 
-          element={<PdfTurnjs {...props} />}
+          element={
+            <Suspense fallback={<>Loading...</>}>
+              <PdfTurnjs {...props} />
+            </Suspense>}
         />
         <Route path='*' element={<div>Not found</div>} />
       </Routes>
